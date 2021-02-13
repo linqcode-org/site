c82af27758ed8cd9197591551965846644cb6d5c
Leonardo Teixeira Timoteo Silva <leonardo.silva@linqcode.com.br>
2021-02-13 19:37:51 -0300

    env

--- pages/api/contato.js
+++ pages/api/contato.js
@@ -29,27 +29,35 @@ export default (req, res) => {
 // async..await is not allowed in global scope, must use a wrapper
 async function main(subject, msg) {
 
+  const host  = process.env.MAIL_HOST;
+  const port  = process.env.MAIL_PORT;
+  const user  = process.env.MAIL_USER;
+  const psw   = process.env.MAIL_PSW;
+  const from = process.env.MAIL_FROM;
+
   // create reusable transporter object using the default SMTP transport
   let transporter = nodemailer.createTransport({
-    host: process.env.HOST,
-    port: process.env.PORT,
+    host: host,
+    port: port,
     secureConnection: false, // true for 465, false for other ports
     auth: {
-      user: process.env.USER, // generated ethereal user
-      pass: process.env.PSW, // generated ethereal password
+      user: user, // generated ethereal user
+      pass: psw, // generated ethereal password
     },
     tls: { ciphers: 'SSLv3' }
   });
 
-  const email = process.env.FROM;
-
   // send mail with defined transport object
   let info = await transporter.sendMail({
-    from: email, // sender address
-    to: email, // list of receivers
+    from: from, // sender address
+    to: from, // list of receivers
     subject: subject, // Subject line
     html: msg, // html body
   });
+
+  // resposta automática para o cliente.
+
+  
 
   console.log("Message sent: %s", info.messageId);
   // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
@@ -58,3 +66,19 @@ async function main(subject, msg) {
   console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
   // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
 }
+
+
+const templateEmail = () => `
+<div style="color: #191c24; font-family: -apple-system, 'Sarabun', sans-serif; font-size: 18px;">
+
+  <h2>Olá,</h2>
+  <p>Agradecemos o seu interesse em nos conhecer, o mais breve entraremos em contato.</p>
+  
+  <p>--</p>
+  <div style="width: 200px">
+    <a target="_blank" href="https://www.linqcode.com.br" style="width: 100px">
+      <img src="https://site-linqcode.linqcode.vercel.app/logo-black.png" alt="Avatar" width="200px" style="width: 200px; max-width: 200px;">
+    </a>
+  </div>
+  
+</div>`
